2706fe2e4829745132a7e0e01910483cbc7219fc
sangjun19 <sangjun0319@gmail.com>
2023-11-27 12:01:01 +0900

    11271200

--- 1018.py
+++ 1018.py
@@ -1,12 +1,13 @@
 n, m = map(int, input().split())
+chess = []
 for i in range(n):
-    chess = list(map(int, input().split()))
+    chess.append(input())
 check = 0
 for i in range(n-1):
     for j in range(n-1):
-        if chess[i][j] != chess[i][j+1] or chess[i][j] != chess[i+1][j]:
-            check += 1   
+        if chess[i][j] == chess[i][j+1] or chess[i][j] == chess[i+1][j]:
+            check += 1
 for j in range(n-1):
-    if chess[i][j] != chess[i][j+1] or chess[i][j] != chess[i-1][j]:
+    if chess[n-1][j] == chess[n-1][j+1] or chess[i][j] == chess[i-1][j]:
         check += 1
 print(check)
