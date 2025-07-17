3d7970a20509fa8b6c00c376abab879c9c0b525d
안상준 <69718646+sangjun19@users.noreply.github.com>
2025-07-17 11:44:48 +0900

    [level 3] Title: 봉인된 주문, Time: 2621.20 ms, Memory: 48.4 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
@@ -1,23 +1,44 @@
 def solution(n, bans):
-    answer = ''
-    bans.sort()
-    cnt = 0
-    for word in bans:
-        temp = list(word)
-        temp.reverse()
-        index = 1
-        arr = []
-        result = 0
-        for w in temp:
-            result += index*(ord(w)-97)
-            index *= 26
-        arr.append(result)
+    def string_to_order(s):
+        order = 0
+        for length in range(1, len(s)):
+            order += 26 ** length
         
-        if result <= n:
-            cnt += 1
-    n -= cnt
-    while n >= 26:
-        answer += chr(n//26 + 97)
-        n = n % 26
+        i = 0
+        for char in s:
+            char_index = ord(char) - 97
+            order += char_index * (26 ** (len(s) - i - 1))
+            i += 1
+        
+        return order + 1
     
-    return answer
+    def order_to_string(order):
+        length = 1
+        total = 0
+        while total + 26 ** length < order:
+            total += 26 ** length
+            length += 1
+        
+        index_in_length = order - total - 1
+        
+        result = ""
+        for i in range(length):
+            digit = index_in_length // (26 ** (length - i - 1))
+            result += chr(ord('a') + digit)
+            index_in_length %= (26 ** (length - i - 1))
+        
+        return result
+
+    ban_orders = []
+    for ban in bans:
+        ban_orders.append(string_to_order(ban))
+    ban_orders.sort()
+    
+    current_n = n
+    for ban_order in ban_orders:
+        if ban_order <= current_n:
+            current_n += 1
+        else:
+            break
+    
+    return order_to_string(current_n)
